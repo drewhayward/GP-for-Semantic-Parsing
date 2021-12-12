8b843027036345cda522dc866e1d735f7129a560
Drew Hayward <d.andrew.hayward@gmail.com>
2021-12-11 22:23:51 -0500

    add plotting

--- allennlp_semparse/state_machines/evolutionary_search.py
+++ allennlp_semparse/state_machines/evolutionary_search.py
@@ -1,17 +1,19 @@
 from copy import deepcopy
-from collections import defaultdict
+from collections import defaultdict, Counter
 import random
 from typing import Dict, List, Tuple, TypeVar
 
 import torch
+import matplotlib.pyplot as plt
 from nltk import Tree as _Tree
+from torch._C import _get_tracing_state
 
 from allennlp_semparse.state_machines import util
 from allennlp_semparse.state_machines import transition_functions
 from allennlp_semparse.state_machines.states import State
 from allennlp_semparse.state_machines.beam_search import Search, BeamSearch
 from allennlp_semparse.state_machines.transition_functions import TransitionFunction
-from allennlp_semparse.domain_languages.domain_language import nltk_tree_to_logical_form, ParsingError
+from allennlp_semparse.domain_languages.domain_language import nltk_tree_to_logical_form
 from tqdm import trange
 
 StateType = TypeVar("StateType", bound=State)
@@ -25,8 +27,42 @@ class Tree(_Tree):
         children_copy = [deepcopy(child) for child in self]
         return Tree(self._label, children_copy, action=self.action)
 
+    def num_leaves(self):
+        if len(self) == 0:
+            return 1
+
+        total = 0
+        for c in self:
+            total += c.num_leaves()
+        return total
+    
+    def __eq__(self, other):
+        return nltk_tree_to_logical_form(self) == nltk_tree_to_logical_form(other)
+
     def __hash__(self):
         return hash(nltk_tree_to_logical_form(self))
+
+class Individual:
+    tree: Tree
+    score: float
+    state: StateType
+    gen_op: str
+    
+    def __init__(self, score: float, tree: Tree, state: StateType, attribution=None):
+        self.tree = tree
+        self.score = score
+        self.state = state
+        if attribution is not None:
+            self.gen_op = attribution
+        else:
+            self.gen_op = 'init'
+    
+    def __eq__(self, other) -> None:
+        return other.tree == self.tree
+
+    def __hash__(self) -> int:
+        return hash(self.tree)
+
 
 def get_children(action: str, ret_non_term = False) -> List[str]:
     """
@@ -104,7 +140,7 @@ def mutate_leaf(program_tree, productions, ratio=1):
 
     return mutated_tree
 
-def mutate_subtree(program_tree, productions, ratio=1):
+def mutate_subtree(program_tree, productions, ratio=1, max_depth=5):
     mutated_tree = deepcopy(program_tree)
     if len(mutated_tree) == 0:
         return mutated_tree
@@ -114,7 +150,7 @@ def mutate_subtree(program_tree, productions, ratio=1):
 
     for node, parent, id, depth in flat_nodes:
         if parent is not None and random.random() < (1 / num_nodes) * ratio: # it's a leaf
-            parent[id] = random_subtree(node.label(), productions, 5)
+            parent[id] = random_subtree(node.label(), productions, max_depth - depth)
 
     return mutated_tree
 
@@ -159,7 +195,7 @@ def crossover_trees(tree1: Tree, tree2: Tree):
 def tournament_selection(pop, k):
     tourney = random.sample(pop, k)
 
-    return max(tourney, key=lambda x: x[0])
+    return max(tourney, key=lambda x: x.score)
 
 def tree_eq(t1, t2):
     return nltk_tree_to_logical_form(t1) == nltk_tree_to_logical_form(t2)
@@ -238,39 +274,87 @@ class EvolutionarySearch(Search):
         if seed_state is not None:
             pop.append(seed_state)
 
-        def fitness(t):
+        def fitness(t, attribution=None):
             score, state = self.eval_program(t, world, trans_func, init_state,action_to_id)
-            return score, t, state
+            return Individual(score, t, state, attribution)
 
         # (tree, score, state) tuples
         pop = [fitness(p) for p in pop]
+        pop_stats = defaultdict(list)
+        pop_stats['ops/cross/mutate_leaf'] = []
+        pop_stats['ops/mutate_subtree'] = []
+        pop_stats['ops/init'] = []
 
         # for each generation
         for gen in trange(self.num_generations):
+            pop = set(pop)
             while len(pop) < self.pop_size + self.pop_lambda:
-                if random.random() < 0.5: # crossover
-                    _, ind1, _ = tournament_selection(pop, self.tournament_k)
-                    _, ind2, _ = tournament_selection(pop, self.tournament_k)
+                if random.random() < 0.3: # crossover
+                    ind1 = tournament_selection(pop, self.tournament_k)
+                    ind2 = tournament_selection(pop, self.tournament_k)
 
-                    cind1, cind2 = crossover_trees(ind1, ind2)
-                    if not tree_eq(ind1, cind1):
-                        pop.extend([fitness(cind1), fitness(cind2)])
+                    cind1, cind2 = crossover_trees(ind1.tree, ind2.tree)
+                    cind1 = mutate_leaf(cind1, productions, self.mutation_ratio)
+                    cind2 = mutate_leaf(cind1, productions, self.mutation_ratio)
+                    if not tree_eq(ind1.tree, cind1):
+                        pop.update([fitness(cind1, 'ops/cross/mutate_leaf'), fitness(cind2, 'ops/cross/mutate_leaf')])
                 else: # mutation
-                    _, ind, _ = tournament_selection(pop, self.tournament_k)
-                    mutated = mutate_subtree(ind, productions)
-                    mutated = mutate_leaf(ind, productions)
-                    if not tree_eq(mutated, ind): # enforce mutations because we are doing mu + lambda
-                        pop.append(fitness(mutated))
+                    ind = tournament_selection(pop, self.tournament_k)
+                    mutated = mutate_subtree(ind.tree, productions, ratio=2, max_depth=self.init_tree_depth)
+                    if not tree_eq(mutated, ind.tree): # enforce mutations because we are doing mu + lambda
+                        pop.add(fitness(mutated, 'ops/mutate_subtree'))
 
             # To get a tree out of a state 's'
             # id_to_action = {i: a for a, i in action_to_id.items()}
             # action_seq = [id_to_action[n] for n in s.action_history[0]]
             # convert action seq to tree with language/world object
-
-            pop.sort(key=lambda x: x[0])
+            pop = list(pop)
+            pop.sort(key=lambda x: x.score)
             pop = pop[-self.pop_size:]
+            
+            # Update population statistics
+            gen_stats = Counter((p.gen_op for p in pop))
+            for k in pop_stats.keys():
+                if not k.startswith('ops/'): continue
+                if k in gen_stats:
+                    pop_stats[k].append(gen_stats[k])
+                else:
+                    pop_stats[k].append(0)
+
+            fits = [p.score for p in pop] 
+            pop_stats['fitness/best'].append(max(fits))
+            pop_stats['fitness/worst'].append(min(fits))
+            pop_stats['fitness/avg'].append(sum(fits)/len(fits))
+
+            pop_stats['inds/avg_depth'].append(sum((p.tree.height() for p in pop))/ self.pop_size)
+            pop_stats['inds/avg_leaves'].append(sum((p.tree.num_leaves() for p in pop))/ self.pop_size)
+
+        # Operation attribution plot
+        plt.clf()
+        plt.subplot(3,1,1)
+        ops = list(k for k in pop_stats.keys() if k.startswith('ops/'))
+        for k in ops:
+            plt.plot(range(self.num_generations), pop_stats[k])
+        plt.ylim(-1, self.pop_size)
+        plt.legend(ops)
+
+        plt.subplot(3,1,2)
+        ops = list(k for k in pop_stats.keys() if k.startswith('fitness/'))
+        for k in ops:
+            plt.plot(range(self.num_generations), pop_stats[k])
+        plt.ylim(-10, 0)
+        plt.legend(ops)
+
+        plt.subplot(3,1,3)
+        ops = list(k for k in pop_stats.keys() if k.startswith('inds/'))
+        for k in ops:
+            plt.plot(range(self.num_generations), pop_stats[k])
+        plt.legend(ops)
+
+
+        plt.savefig(f'figs/{0}attrib.png')
         
-        return [p[2] for p in pop[-5:]]
+        return [p.state for p in pop[-5:]]
 
     def get_seed_state(self, num_steps, initial_state, trans_func, action_to_id, world):
         id_to_action = {i: a for a, i in action_to_id.items()}
@@ -294,6 +378,7 @@ class EvolutionarySearch(Search):
         action_to_id = {act.rule: i for i, act in enumerate(actions[0])}
         world = world[0]
         
+        # try:
         # seed_tree = self.get_seed_state(num_steps, initial_state, transition_function, action_to_id, world)
         top_k = self.single_evo_search(world, initial_state, transition_function, action_to_id, None)
 
@@ -301,3 +386,6 @@ class EvolutionarySearch(Search):
         # transition_function.take_step(initial_state, allowed_actions=[{0},{0},{0},{0},{0},{0},{0},{0},{0},{0}])
         best_states: Dict[int, List[StateType]] = {0: top_k}
         return best_states
+        # except:
+        #     print('Failed to search for the parse')
+        #     return {}
